62681519a07194b1fe5676f2aaf3dfc870770dce
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-03-26 14:48:42 -0300

    Registro de usuario

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+//verifica se um usuario com mesmo username já existe
+function userExists(username, users) {
+  return users.some(user => user.username === username);
+}
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;  
+
+  //chamada da função de verificação de existencia do usuario
+  if (userExists(username,users)) {
+      return response.status(400).json({ error: "User already exists!" });
+  }   
+
+  //inserção do usuario
+  users.push({ 
+    username, 
+    name,
+    id: uuidv4,
+    todos: []
+  })
+
+  //resposta de sucesso
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
